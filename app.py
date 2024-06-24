908488262a5ea88d870f6f265b9f1c2844349a86
arvindk1 <arvindsjunk@gmail.com>
2024-06-24 10:08:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title="Annuity Master Bot", page_icon=None, layout="wide
 
 #us-west-2
 AGENT_ID = 'KXWRFB462G'
-AGENT_ALIAS_ID = 'MLYIL20IOO'
+AGENT_ALIAS_ID = 'EBTIJBBLSC'
 
 # Function to get AWS credentials
 def get_aws_credentials():
